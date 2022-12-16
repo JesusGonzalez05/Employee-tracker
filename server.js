343f3d02d9c5e2a47e3ba9760f5b4801e6b4a474
JesusGonzalez05 <Jesusgonzalez051698@gmail.com>
2022-12-16 08:25:58 -0500

    connect to db.

--- server.js
+++ server.js
@@ -12,3 +12,17 @@ const app = express();
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      password: 'rootroot',
+      database: 'company_db'
+    },
+    console.log(`Connected to the company_db database.`)
+    
+  );
